bdf6fd5e2760c6046eaefb1a5efe7b29f8a9cff9
JobGetabu <getabujob@gmail.com>
2018-09-19 13:43:28 +0300

    first commit

---
+++ app/src/main/java/com/job/sbl/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.job.sbl.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.job.sbl.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
